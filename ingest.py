135812c3d4929f81e7f1bddf9beca86ca7ef66d1
peter <ashu0000008@163.com>
2023-07-26 16:46:16 +0800

    添加百度unit的faq以测试效果

--- ingest.py
+++ ingest.py
@@ -1,7 +1,7 @@
 """Load html from files, clean up, split, ingest into Weaviate."""
 import pickle
 
-from langchain.document_loaders import ReadTheDocsLoader
+from langchain.document_loaders import ReadTheDocsLoader, PyPDFLoader, TextLoader, DirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
@@ -9,15 +9,20 @@ from langchain.vectorstores.faiss import FAISS
 
 def ingest_docs():
     """Get documents from web pages."""
-    loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    # loader = ReadTheDocsLoader("langchain.readthedocs.io/en/latest/")
+    # loader = PyPDFLoader("PAXPAY互联网终端接入规范V2.1.pdf")
+    # loader = TextLoader("faqskill_FAQ_百度_UNIT.txt", autodetect_encoding=True)
+
+    loader = DirectoryLoader('./faq_baidu_unit', glob='**/*.txt')
     raw_documents = loader.load()
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200,
+        chunk_size=400,
+        chunk_overlap=100,
     )
     documents = text_splitter.split_documents(raw_documents)
     embeddings = OpenAIEmbeddings()
     vectorstore = FAISS.from_documents(documents, embeddings)
+    print("end")
 
     # Save vectorstore
     with open("vectorstore.pkl", "wb") as f:
